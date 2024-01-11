5bd5bcd28264f9aa8f66b953e8c166289d122f39
codewithrishabhh~ <rishabhsahu6196@gmail.com>
2024-01-11 13:38:27 +0530

    dom

---
+++ app.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("div");
+console.log(div)
+div.style.height = "200px"
+div.style.width = "300px"
+div.style.backgroundColor = "yellow"
+div.style.margin ="20px"
+div.style.padding = "10px"
+div.style.fontSize = "18px"
+div.style.fontWeight = "bold"
+
